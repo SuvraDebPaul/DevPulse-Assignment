4d3bc50f0e1d4b010c337290932574ffb3432c81
Suvra Deb Paul | Web Developer <suvra.deb.paul@gmail.com>
2026-05-21 01:17:43 +0600

    Modified Global Error Middleware

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,19 +1,30 @@
-import type { ErrorRequestHandler } from "express";
+import type {
+  ErrorRequestHandler,
+  NextFunction,
+  Request,
+  Response,
+} from "express";
 import config from "../config";
 
 export const globalErrorHandler: ErrorRequestHandler = (
-  error,
-  req,
-  res,
-  next,
+  error: unknown,
+  req: Request,
+  res: Response,
+  next: NextFunction,
 ) => {
-  const statusCode = error.statusCode || 500;
-  const message = error.message || "Something Went Wrong";
+  const userStatusCode = error as { statusCode?: number };
+  const statusCode = userStatusCode.statusCode || 500;
 
   res.status(statusCode).json({
     success: false,
-    message: message,
-    error,
-    stack: config.node_env === "development" ? error.stack : undefined,
+    message: error instanceof Error ? error.message : "Something Went Wrong",
+    error:
+      config.node_env === "development" && error instanceof Error
+        ? error
+        : "Something Went Wrong",
+    stack:
+      config.node_env === "development" && error instanceof Error
+        ? error.stack
+        : undefined,
   });
 };
